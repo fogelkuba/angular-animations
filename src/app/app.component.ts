0ba6755aa78793ac1062e382457833888ff78da7
fogelkuba <fogelkuba@gmail.com>
2018-08-19 18:22:51 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,39 @@ import {trigger, state, style, transition, animate} from "@angular/animations";
                 backgroundColor: 'blue',
                 transform: 'translateX(100px)'
             })),
-            transition('normal => highlighted', animate(250)),
-            transition('highlighted => normal', animate(100)),
-        ])
+            transition('normal <=> highlighted', animate(1500)),
+            // transition('highlighted => normal', animate(100)),
+        ]),
+        trigger('wildState', [
+            state('normal', style({
+                backgroundColor: 'green',
+                transform: 'translateX(0) scale(1)'
+            })),
+            state('highlighted', style({
+                backgroundColor: 'blue',
+                transform: 'translateX(100px) scale(1)'
+            })),
+            state('shrunken', style({
+                backgroundColor: 'azure',
+                transform: 'translateX(100px) scale(0.5)'
+            })),
+            transition('normal <=> highlighted', animate(1500)),
+        ]),
     ]
 })
 export class AppComponent {
     state = 'normal';
+    wildState = 'normal';
     list = ['Milk', 'Sugar', 'Bread'];
 
     onAnimate() {
         this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
         console.log(this.state);
+    }
+
+    onShrink() {
+        this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+        console.log(this.wildState);
     }
 
     onAdd(item) {
